dba30c06fcd08e2b5c598b91739d1e88deab5c2a
Hamza <Hamza5955>
2021-09-19 23:59:06 +0200

    bonus

--- testlive/src/App.vue
+++ testlive/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld />
+    <MonaLisa />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Monalisa from "./components/monalisa.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Monalisa,
   },
 };
 </script>
---
+++ testlive/src/components/MonaLisa.vue
@@ -0,0 +1,12 @@
+<template>
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Leonardo_da_Vinci_-_Mona_Lisa.jpg/1200px-Leonardo_da_Vinci_-_Mona_Lisa.jpg"
+    alt=""
+  />
+</template>
+
+<script>
+export default {
+  name: "MonaLisa",
+};
+</script>
